286ace4a0cfdf5621a890564a395a0d199fc2d9d
lewistrotter <lewistrotter@postgrad.curtin.edu.au>
2021-05-27 06:55:15 +0000

    Created new module for ensemble model

---
+++ modules/ensemble.py
@@ -0,0 +1,13 @@
+# ensemble
+"""
+"""
+
+# import required libraries
+import os, sys
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+#sys.path.append('../../shared')
+#import satfetcher, tools
+
