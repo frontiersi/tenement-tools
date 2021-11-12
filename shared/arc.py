c3f8e4bb367e46dafb374ab6bfbc11bf5c18aeb9
Lewis Trotter <53249726+lewistrotter@users.noreply.github.com>
2021-11-12 22:42:32 +0800

    Fixed ensemble model

--- shared/arc.py
+++ shared/arc.py
@@ -457,46 +457,59 @@ def convert_ensemble_parameters(in_lyrs):
         a, bc, d = lyr[3], lyr[4], lyr[5]
 
         # clean a
-        if a == '' or a.lower() == 'n/a':
+        if a.lower() == 'na':
             a = None
         elif a.lower() == 'min':
             a == 'Min'
         elif a.lower() == 'max':
             a == 'Max'
+        elif a.isnumeric():
+            a = float(a)
         else:
-            try:
-                a = float(a)
-            except:
-                raise ValueError('Value for a is not a numeric.')
+            raise ValueError('Value for a is not supported.')
 
         # clean bc
-        if bc == '' or bc.lower() == 'n/a':
+        if bc.lower() == 'na':
             bc = None
-        elif bc.lower() in ['min', 'max']:
-            raise ValueError('Can not set mid point to Min or Max.')
+        elif bc.lower() == 'min':
+            bc == 'Min'
+        elif bc.lower() == 'max':
+            bc == 'Max'
+        elif bc.isnumeric():
+            bc = float(bc)
         else:
-            try:
-                bc = float(bc)
-            except:
-                raise ValueError('Value for bc is not a numeric.')
+            raise ValueError('Value for bc is not supported.')
 
         # clean d
-        if d == '' or d.lower() == 'n/a':
+        if d == '' or d.lower() == 'na':
             d = None
         elif d.lower() == 'min':
             d == 'Min'
         elif d.lower() == 'max':
             d == 'Max'
+        elif d.isnumeric():
+            d = float(d)
         else:
-            try:
-                d = float(d)
-            except:
-                raise ValueError('Value for d is not a float.')  
+            raise ValueError('Value for d is not supported.')
+            
+        # check if two nones in list. max is 1
+        num_none = sum(i is None for i in [a, bc, d])
+        if num_none > 1:
+            raise ValueError('Signoidals do not support two NA values.')
+            
+        # check if two min in list. max is 1
+        num_min = sum(str(i) == 'Min' for i in [a, bc, d])
+        if num_min > 1:
+            raise ValueError('Signoidals do not support two Min values.')
 
-
+        # check if two max in list. max is 1
+        num_max = sum(str(i) == 'Max' for i in [a, bc, d])
+        if num_max > 1:
+            raise ValueError('Signoidals do not support two Max values.')                  
+        
         # append
         clean_lyrs.append([lyr[0].value, lyr[1], lyr[2], a, bc, d])
-    
+            
     # gimme
     return clean_lyrs
 
